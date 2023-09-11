0925697fb68f8e55ad6a9fec240815f314655b41
nonamed006 <jjdmst@gmail.com>
2023-09-11 17:44:54 +0900

    사원관리 컴포넌트 추가

--- src/views/admin/test/component/EmpCard.js
+++ src/views/admin/test/component/EmpCard.js
@@ -3,8 +3,10 @@ import {
   Box,
   Button,
   Flex,
+  Spacer,
   Table,
   Tbody,
+  Td,
   Text,
   Th,
   Thead,
@@ -14,15 +16,18 @@ import {
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { PORT } from "set";
+import { minTimeDate } from "common";
 
 const EmpCard = () => {
   const textColor = useColorModeValue("secondaryGray.900", "white");
+  const textNumColor = useColorModeValue("brand.500", "white");
 
   //테이블 헤더
   const headerGroups = ["이름", "ID", "최초입사일"];
 
   //state
   const [emp, setEmp] = useState([]);
+  const [empNum, setEmpNum] = useState();
 
   const getEmpList = (searchCorp, searchWorkType, searchNm) => {
     fetch(`${PORT}/emp/getEmp/${searchCorp}/${searchWorkType}/${searchNm}`, {
@@ -32,12 +37,24 @@ const EmpCard = () => {
       .then((res) => res.json())
       .then((res) => {
         console.log(res);
-        setEmp(res);
+        setEmp(res.data);
+        setEmpNum(res.strData);
       });
   };
 
+  const getEmpNum = (empNum) =>{
+    fetch(`${PORT}/emp/getEmpDetail/${empNum}`, {
+        method: "GET",
+        // res에 결과가 들어옴
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res);
+        });
+  }
+
   useEffect(() => {
-    getEmpList("noSearch","noSearch","noSearch");
+    getEmpList("noSearch", "noSearch", "noSearch");
   }, []);
 
   // const tableInstance = useTable(
@@ -52,10 +69,9 @@ const EmpCard = () => {
 
   return (
     <div>
-      <Box borderRadius="lg" bg="white" h="600px" p="6">
+      <Box borderRadius="lg" bg="white" h="700px" p="6">
         <Flex
           align={{ sm: "flex-start", lg: "center" }}
-          justify="space-between"
           w="100%"
           px="22px"
           pb="20px"
@@ -68,8 +84,25 @@ const EmpCard = () => {
             fontWeight="700"
             lineHeight="100%"
           >
-            사용자
+            사용자 
           </Text>
+          <Text
+            color={textNumColor}
+            fontSize="22px"
+            fontWeight="700"
+            lineHeight="100%"
+          >
+            {empNum}
+          </Text>
+          <Text
+            color={textColor}
+            fontSize="22px"
+            fontWeight="700"
+            lineHeight="100%"
+          >
+            명
+          </Text>
+          <Spacer />
           <Button variant="action">추가</Button>
         </Flex>
         <Table variant="simple" color="gray.500">
@@ -89,15 +122,47 @@ const EmpCard = () => {
               ))}
             </Tr>
           </Thead>
-          <Tbody>
-            <Tr>
-              <Flex align="center">
-                <Avatar src="" w="30px" h="30px" me="8px" />
-                <Text color={textColor} fontSize="sm" fontWeight="600">
-                  이름
-                </Text>
-              </Flex>
-            </Tr>
+          <Tbody >
+            {emp.map((column, index) => (
+              <Tr onClick={()=>{
+                getEmpNum(column.empCd)
+              }} >
+                <Td
+                  fontSize={{ sm: "14px" }}
+                  minW={{ sm: "150px", md: "200px", lg: "auto" }}
+                  borderColor="transparent"
+                >
+                  <Flex align="center">
+                    <Avatar src="" w="30px" h="30px" me="8px" />
+                    <Text color={textColor} fontSize="sm" fontWeight="600">
+                      {column.empNm}
+                    </Text>
+                  </Flex>
+                </Td>
+                <Td
+                  fontSize={{ sm: "14px" }}
+                  minW={{ sm: "150px", md: "200px", lg: "auto" }}
+                  borderColor="transparent"
+                >
+                  <Flex align="center">
+                    <Text color={textColor} fontSize="sm" fontWeight="600">
+                    {column.empId}
+                    </Text>
+                  </Flex>
+                </Td>
+                <Td
+                  fontSize={{ sm: "14px" }}
+                  minW={{ sm: "150px", md: "200px", lg: "auto" }}
+                  borderColor="transparent"
+                >
+                  <Flex align="center">
+                    <Text color={textColor} fontSize="sm" fontWeight="600">
+                    {minTimeDate(column.joinDt)}
+                    </Text>
+                  </Flex>
+                </Td>
+              </Tr>
+            ))}
           </Tbody>
         </Table>
       </Box>
