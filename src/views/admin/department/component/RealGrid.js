c3dd6b73a6c9575a3b71943ee4b91f96c5e3d0df
NameYeol99 <skaduf73@gmail.com>
2023-09-14 16:30:37 +0900

    Grid 와 부서정보조회

--- src/views/admin/department/component/RealGrid.js
+++ src/views/admin/department/component/RealGrid.js
@@ -2,7 +2,10 @@ import { useEffect, useRef, useState } from "react";
 import {LocalTreeDataProvider,TreeView  } from "realgrid";
 // import { rows } from "./realgrid-data";
 import realgridjson from "../realgrid"
+
 import "../css/realgrid-style.css"; // RealGrid CSS 추가
+import {PORT} from "set";
+import { useSelector } from "react-redux";
 
 
 
@@ -10,35 +13,35 @@ function RealGrid() {
   const [dataProvider, setDataProvider] = useState(null);
   const [gridView, setGridView] = useState(null);
   const realgridElement = useRef(null);
+  const [getOrg, setGetOrg] = useState([]);
+  const org = useSelector((state) => state.solution.dataPk);
+  console.log(org);
 
   var fields = [
-    {fieldName:"area1code", dataType:"text"},
-    {fieldName:"area1name", dataType:"text"},
-    {fieldName:"area2code", dataType:"text"},
-    {fieldName:"area2name", dataType:"text"},
-    {fieldName:"area3code", dataType:"text"},
-    {fieldName:"area3name", dataType:"text"},
-    {fieldName:"treeId", dataType:"text"},
-    {fieldName:"treeName", dataType:"text"},
+    {fieldName:"dpPath", dataType:"text"},
+    {fieldName:"dpNm", dataType:"text"},
     {fieldName:"iconField", dataType:"text"}
   ]
   
   var columns = [
-    {fieldName:"treeName", name:"treeName", width: 150, header:{text:"지역명"}},
-    {fieldName:"treeId", name:"treeId", header:{text:"TreeID"}},
-    {fieldName:"area1code", name:"area1code", header:{text:"시도코드"}},
-    {fieldName:"area1name", name:"area1name", header:{text:"시도명"}},
-    {fieldName:"area2code", name:"area2code", header:{text:"시군구코드"}},
-    {fieldName:"area2name", name:"area2name", header:{text:"시군구명"}},
-    {fieldName:"area3code", name:"area3code", header:{text:"읍면동코드"}},
-    {fieldName:"area3name", name:"area3name", header:{text:"읍면동명"}},
+    {fieldName:"dpNm", name:"dpNm", width: 150, header:{text:"명칭"}},
+    {fieldName:"dpPath", name:"dpPath", header:{text:"dpPath"}},
     {fieldName:"iconField", name:"iconField"}
   ];
- 
-
+  const searchOrg = () => {
+		let url = `${PORT}/dep?text=부&coNm=string`;
+		fetch(url, {method : "GET" })	
+			.then(res=>res.json())
+			.then(res=>{
+        console.log(res)
+        
+				treeProvider.setRows(res, 'dpPath', true, null, "iconField");
+      })
+  };
 
   var treeProvider, treeView;
   useEffect(() => {
+  searchOrg();
   const container = realgridElement.current;
   treeProvider = new LocalTreeDataProvider();
   treeView = new TreeView(container);
@@ -52,6 +55,7 @@ function RealGrid() {
   treeView.header.height = 40;
   treeView.footer.height = 40;
   treeView.stateBar.width = 16;
+  
   treeView.treeOptions.iconImagesRoot = "/horizon-ui-chakra/img/";
   treeView.treeOptions.iconImages = [
        "kr.png", "de.png", "es.png", "fr.png", "cor.png", "icon2.png", "is.png",
@@ -60,14 +64,16 @@ function RealGrid() {
    
    let data;
    console.log(realgridjson)
+   console.log(getOrg)
+   
    treeView.onCurrentChanged = function (grid, clickData) {
     data = clickData.dataRow;
     console.log(data)
     
     console.log(realgridjson[data])
+   
   }
 
-  treeProvider.setRows(realgridjson, 'treeId', true, null, "iconField");
   return () => {
 
       treeProvider.clearRows();
--- src/views/admin/department/component/SearchCardBar.js
+++ src/views/admin/department/component/SearchCardBar.js
@@ -1,26 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Button, Select, Grid, Input, GridItem} from '@chakra-ui/react';
 import { SearchBar } from 'components/navbar/searchBar/SearchBar';
-
+import { useDispatch } from 'react-redux';
+import { setDataPk } from 'redux/solution';
+import {PORT} from "set";
 const SearchCardBar = () => {
+
+	const [corpNm, setCorpNm] = useState([]);
+	const [selectedCoNm, setSelectedCoNm] = useState('');
+	const [searchText, setSearchText] = useState('');
+	const [depCd, setDepCd] = useState();
+
+	//리덕스
+	const dispatch = useDispatch();
+
+	const getCorpNmList = () => {
+		let url = `${PORT}/corp/list`;
+		fetch(url, {
+			method : "GET"
+		}).then(res=>res.json())
+			.then(res=>{
+				setCorpNm(res.data);
+				
+			});
+			     
+	};
+	const coNmChange = (e) => {
+		const selectedValue = e.target.value;
+    setSelectedCoNm(selectedValue);
+	}
+	const onChangeSearchText = (e) => {
+		setSearchText(e.target.value);
+	}
+	const onClickSearchText = () => {
+		let url = `${PORT}/dep?text=${searchText}&coNm=${selectedCoNm}`;
+		fetch(url, {method : "GET" })	
+			.then(res=>res.json())
+			.then(res=>{
+				dispatch(setDataPk(res));
+				console.log(res)
+				
+			});
+	}
+	useEffect(() => {
+    getCorpNmList();
+  }, []);
+
 	return (<div>
 		<Box borderRadius='lg' bg='white' p='6'>
 			<Grid templateColumns='repeat(14, 1fr)' gap={2}>
 				<GridItem colSpan={1}><div style={{textAlign: 'center'}}>회사</div></GridItem>
 				<GridItem colSpan={2}>
-					<Select placeholder='전체'>
-						<option value='option1'>Option 1</option>
-						<option value='option2'>Option 2</option>
-						<option value='option3'>Option 3</option>
+					<Select placeholder='전체' value={selectedCoNm} onChange={coNmChange}>
+						{corpNm.map((item, index) => (
+            	<option key={index} value={item}>
+              	{item}
+            	</option>
+          ))}
 					</Select>
 				</GridItem>
 				
 				<GridItem colStart={5} colEnd={5}><div style={{textAlign: 'center'}}>코드/부서명</div></GridItem>
 				<GridItem colSpan={3}>
-					<Input placeholder="검색어를 입력하세요." size="md" borderRadius="14px" />
+					<Input placeholder="검색어를 입력하세요." name="searchText" size="md" borderRadius="14px" onChange={onChangeSearchText} />
 				</GridItem>
 				<GridItem colStart={14} colEnd={14}>
-					<Button variant="brand">검색</Button>
+					<Button variant="brand" onClick={onClickSearchText}>검색</Button>
 				</GridItem>
 			</Grid>
 		</Box>
