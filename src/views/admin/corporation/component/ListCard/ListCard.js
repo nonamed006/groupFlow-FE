1698efb432ec38753288c7809663c06c16809992
EunbiAn <68534747+EunbiAn@users.noreply.github.com>
2023-10-17 19:32:18 +0900

    회사목록 페이징 추가

--- src/views/admin/corporation/component/ListCard/ListCard.js
+++ src/views/admin/corporation/component/ListCard/ListCard.js
@@ -1,15 +1,75 @@
 import { Box } from "@chakra-ui/react/dist/chakra-ui-react.cjs";
 import React from "react";
 import ListCardTable from "./ListCardTable";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import CardMenuBar from "common/component/CardMenuBar";
-const ListCard = ({ title, setCoCd, listData, changeYn, initCorpList, coCd }) => {
+import { useInView } from 'react-intersection-observer';
+import { SyncLoader } from "react-spinners"
+import { PORT } from "set";
+const ListCard = ({ keyword, useYn, title, setCoCd, changeYn, coCd }) => {
+  const [corpList, setCorpList] = useState([]); // 회사데이터 목록
+  const [init, setInit] = useState(); // 첫로딩, 검색시 초기화
 
-  useEffect(() => { // 저장, 삭제, 수정 등의 이벤트가 있을 때 다시 값 가져오기
-    if (changeYn) {
-      initCorpList();
-    }
+  const [currentPage, setCurrentPage] = useState(1);   // 현재페이지
+  const [isLastPage, setIsLastPage] = useState(false);  // 마지막페이지 여부
+  const [totalCount, setTotalCount] = useState(); // 총 데이터 갯수
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [infiniteScrollRef, inView] = useInView();
+
+  useEffect(() => {  // 값 초기화
+    handleSearchBtn();
   }, [changeYn]);
+
+  useEffect(() => {
+    fetchCorpList();
+  }, [init]);
+
+  // 검색 버튼 클릭 시
+  const handleSearchBtn = async () => { // 초기화 
+    setCorpList([]);
+    setIsLastPage(false);
+    setCurrentPage(1);
+    setTotalCount(0);
+    setInit(!init);
+  };
+
+  useEffect(async () => {
+    if (inView && !isLastPage) {
+      await setIsLoading(true);
+      fetchCorpList();
+      await setIsLoading(false);
+    }
+  }, [inView]);
+
+
+  // 회사 목록 조회 및 검색
+  const fetchCorpList = async () => {
+    let url = `${PORT}/corp`;
+    // URL 파라미터 생성
+    const params = new URLSearchParams();
+    if (keyword !== "") params.append("keyword", keyword);
+    if (useYn !== "") params.append("useYn", useYn);
+    params.append("pageNum", currentPage);
+    // URL에 파라미터 추가
+    const paramString = params.toString();
+    if (paramString) {
+      url += "?" + paramString;
+    }
+
+    fetch(url, {
+      method: "GET",
+    }).then((res) => res.json())
+      .then((res) => {
+        if (res.result === 'success') {
+          setCorpList([...corpList, ...(res.pageInfo.list)]);
+          setTotalCount(res.pageInfo.total);
+          setIsLastPage(res.pageInfo.isLastPage);
+          setCurrentPage((currentPage) => currentPage + 1);
+        }
+      });
+    return;
+  };
 
   return (
     <Box borderRadius="lg" bg="white" h="700px" p="6">
@@ -17,12 +77,20 @@ const ListCard = ({ title, setCoCd, listData, changeYn, initCorpList, coCd }) =>
       <CardMenuBar
         handelOnClik={setCoCd}
         title={title}
-        count={listData !== undefined ? listData.length : 0}
+        count={totalCount}
         buttonType={true}
         btnText={'추가'} />
       {/* 목록 테이블 */}
-      <Box w={'100%'} display={'inline-block'} overflowX={"auto"} overflowY={"auto"} h={'90%'} >
-        <ListCardTable listData={listData} setCoCd={setCoCd} coCd={coCd}/>
+      <Box w={'100%'} display={'inline-block'} overflowX={"auto"} overflowY={"auto"} h={'550px'} >
+        <Box minH={'560px'}>
+          <ListCardTable listData={corpList} setCoCd={setCoCd} coCd={coCd} />
+        </Box>
+
+        <Box ref={infiniteScrollRef} bg={'white'} w={'100%'} h={'1px'}></Box>
+        {isLoading &&
+          <SyncLoader />
+        }
+
       </Box>
 
     </Box>
--- src/views/system/role/component/Corp/CorpList/CorpList.js
+++ src/views/system/role/component/Corp/CorpList/CorpList.js
@@ -6,25 +6,43 @@ import CardMenuBar from "common/component/CardMenuBar";
 import { PORT } from "set";
 import ListCardTable from "views/admin/corporation/component/ListCard/ListCardTable";
 import SearchBar from "common/component/SearchBar";
+import { useInView } from 'react-intersection-observer';
+import { SyncLoader } from "react-spinners"
 
 const CorpList = ({ setCoCd, coCd }) => {
 
-  const [corpList, setCorpList] = useState();
-  const [keyword, setKeyword] = useState();
+  const [corpList, setCorpList] = useState([]); // 회사데이터 목록
+  const [keyword, setKeyword] = useState(); // 검색어
+  
+  const [currentPage, setCurrentPage] = useState(1);   // 현재페이지
+  const [isLastPage, setIsLastPage] = useState(false);  // 마지막페이지 여부
+  const [totalCount, setTotalCount] = useState(); // 총 데이터 갯수
 
-  useEffect(() => {
-    fetchCorpList();  // 회사목록 조회
-  }, []);
+  const [init, setInit] =useState(); // 첫로딩, 검색시 초기화
+  const [isLoading, setIsLoading] = useState(false);
+  const [infiniteScrollRef, inView] = useInView();
 
-  // 회사목록 조회
-  const fetchCorpList = () => {
+  useEffect( () => {
+    fetchCorpList();
+  }, [init]);
+
+  useEffect(async () => {
+    if (inView && !isLastPage ) {
+     // await setIsLoading(true);
+      fetchCorpList();
+      //setIsLoading(false);
+    }
+  }, [inView]);
+
+
+  // 회사 목록 조회 및 검색
+  const fetchCorpList = async () => {
     let url = `${PORT}/corp`;
 
     // URL 파라미터 생성
     const params = new URLSearchParams();
-    if (keyword !== undefined && keyword !== 'undefined')
-      params.append("keyword", keyword);
-
+    if (keyword !== undefined && keyword !== 'undefined') params.append("keyword", keyword);
+    params.append("pageNum", currentPage);
     // URL에 파라미터 추가
     const paramString = params.toString();
     if (paramString) {
@@ -36,15 +54,23 @@ const CorpList = ({ setCoCd, coCd }) => {
     })
       .then((res) => res.json())
       .then((res) => {
-
-        setCorpList(res.data);
-        setCoCd(undefined);
+        if (res.result === 'success') {
+          setCorpList([...corpList, ...(res.pageInfo.list)]);
+          setTotalCount(res.pageInfo.total);
+          setCoCd(undefined);
+          setIsLastPage(res.pageInfo.isLastPage);
+          setCurrentPage((currentPage) => currentPage + 1);
+        }
       });
   };
 
   // 검색 버튼 클릭 시
-  const handleSearchBtn = () => {
-    fetchCorpList();
+  const handleSearchBtn =  () => { // 초기화 
+    setCorpList([]);
+    setIsLastPage(false);
+    setCurrentPage(1);
+    setTotalCount(0);
+    setInit(!init);
   };
 
   return (
@@ -52,13 +78,19 @@ const CorpList = ({ setCoCd, coCd }) => {
       {/* 목록 상단 */}
       <CardMenuBar
         title={'회사'}
-        count={corpList !== undefined ? corpList.length : 0}
+        count={totalCount}
         buttonType={false}
       />
-       <SearchBar setKeyword={setKeyword} handleSearchBtn={handleSearchBtn} placeholder={'회사명 입력하세요'} btnText={'검색'} />
+      <SearchBar setKeyword={setKeyword} handleSearchBtn={handleSearchBtn} placeholder={'회사명 입력하세요'} btnText={'검색'} />
       {/* 목록 테이블 */}
-      <Box w={'100%'} display={'inline-block'} overflowX={"auto"} height={'550px'} overflowY={"auto"} >
-        <ListCardTable listData={corpList} setCoCd={setCoCd} coCd={coCd} />
+      <Box w={'100%'} display={'inline-block'} overflowY={"auto"} height={'550px'} >
+        <Box  minH={'560px'} >
+            <ListCardTable listData={corpList} setCoCd={setCoCd} coCd={coCd} />
+        </Box>
+        {!isLoading ?
+        <Box ref={infiniteScrollRef} bg={'white'} w={'100%'} h={'1px'} ></Box>
+          :<SyncLoader />
+        }
       </Box>
 
     </Box>
