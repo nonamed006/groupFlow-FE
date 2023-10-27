2dd581123da110db51c80f851a8daf39fc364daa
EunbiAn <68534747+EunbiAn@users.noreply.github.com>
2023-10-27 14:51:05 +0900

    권한그룹메뉴 수정 시 전체/관리자/사용자 구분

--- src/views/system/roleGroup/component/MenuBox/TotalMenuBox.js
+++ src/views/system/roleGroup/component/MenuBox/TotalMenuBox.js
@@ -1,18 +1,21 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import RealGrid from "../RealGrid";
 import api from "api/Fetch";
+import Loading from "common/Loading";
 
-const TotalMenuBox = ({ rgCd, setCheckedMenuCd }) => {
+const TotalMenuBox = ({ rgCd, setCheckedMenuCd, typeCd }) => {
     const [menuList, setMenuList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     useEffect(() => {
         fetchMenuList();
     }, [])
 
     // 메뉴 전체 조회 + 권한그룹의 메뉴일 경우 체크여부 포함
     const fetchMenuList = async () => {
-        let res = await api.roleMenu.getMenuListWithRole(rgCd);
-        if ( res.status === 200 ) {
+        setIsLoading(true);
+        let res = await api.roleMenu.getMenuListWithRole(rgCd, typeCd);
+        if (res.status === 200) {
             setMenuList(res.data);
             let menuCds = [];
             res.data &&
@@ -21,14 +24,29 @@ const TotalMenuBox = ({ rgCd, setCheckedMenuCd }) => {
                         menuCds.push(element.menuCd);
                 });
             setCheckedMenuCd(menuCds);
-        } else{
+        } else {
             setMenuList();
             setCheckedMenuCd();
         }
+        setIsLoading(false);
     }
     return (
-        <Box borderRadius="lg" bg="white" h="fit-content" display={'flex'} px="6">
-            <RealGrid type={'modify'} org={menuList} setCheckedMenuCd={setCheckedMenuCd} />
+        <Box borderRadius="lg" bg="white" h="fit-content" display={'flex'} px="6" minH={'500px'}>
+            {isLoading ?
+                <Loading />
+                :
+                menuList.length > 0 ?
+                    <RealGrid type={'modify'} org={menuList} setCheckedMenuCd={setCheckedMenuCd} />
+                    :
+                    <Text
+                        pt={200}
+                        align={'center'}
+                        fontWeight={600}
+                        color={'lightgray'}
+                        fontSize={'18px'}
+                    >
+                        검색된 데이터가 없습니다.</Text>
+            }
         </Box>
     );
 };
