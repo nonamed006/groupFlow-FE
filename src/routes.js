7766d786d81dcc8f63c3cf3b9b1e6aadf1645ecc
EunbiAn <68534747+EunbiAn@users.noreply.github.com>
2023-11-13 10:49:21 +0900

    로고클릭시 홈화면 이동 추가

--- src/routes.js
+++ src/routes.js
@@ -139,7 +139,7 @@ const routes = [
     component: LogInCentered,
   },
   {
-    name: "HOME",
+    name: "GROUP FLOW",
     layout: "/system",
     path: "/home",
     icon: <Icon as={MdLock} width="20px" height="20px" color="inherit" />,
