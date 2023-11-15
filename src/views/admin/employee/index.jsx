183dfc41b0819e26b0cf1ecbc3a3ff37ee306f68
jej <jjdmst@gmail.com>
2023-11-15 18:30:34 +0900

    프로필 변경 팝업 반영

--- src/views/admin/employee/index.jsx
+++ src/views/admin/employee/index.jsx
@@ -238,15 +238,10 @@ const Employee = () => {
   }, [isReload]);
 
   return (
-    <div>
       <Box h={'full'}>{/* pt={{ base: "150px", md: "100px", xl: "100px" }} 혜윤 수정 */}
         <Grid
-<<<<<<< HEAD
           //h="1000px"
           h={'full'} // 혜윤 수정
-=======
-          h="full"
->>>>>>> jej
           templateRows="repeat(11, 1fr)"
           templateColumns="repeat(6, 1fr)"
           gap={5}
@@ -299,7 +294,6 @@ const Employee = () => {
 			}
 
       </Box>
-    </div>
   );
 };
 
